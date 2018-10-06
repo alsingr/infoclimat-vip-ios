bc60fae11d544fd1114686d7e28de46c7f26d914
Alvyn Silou <alvyn.silou@groupama.com>
2018-10-06 13:47:17 +0200

    Project Setup with VIP Pattern

---
+++ WeatherApp/Scenes/WeatherForecasts/WeatherForecastsTableView.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherForecastsViewController.swift
+//  WeatherApp
+//
+//  Created by Alvyn SILOU on 06/10/2018.
+//  Copyright © 2018 Alvyn SILOU. All rights reserved.
+//
+
+import UIKit
+
+extension WeatherForecastsViewController {
+
+  // MARK: - Table View
+
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 2
+  }
+
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    cell.textLabel!.text = "object.description"
+    return cell
+  }
+
+}
+
